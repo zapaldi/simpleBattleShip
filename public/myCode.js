70f8089dd1b773cc291064ac82ccfd4ae0ba84c0
Diana Zapalac <dlzapalac@gmail.com>
2021-03-21 19:59:34 +0000

    added myCode.js to project

---
+++ public/myCode.js
@@ -0,0 +1,8 @@
+	var numWarnings = 0;
+        while ( numWarnings < 5) {
+	   alert("Still some warnings left")
+	   numWarnings++;
+	}
+	alert("All Your Warnings Have Been Used!")
+	console.log ("Howdy Partner")
+
