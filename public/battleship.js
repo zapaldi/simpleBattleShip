7cacfc1fb07617c5d6a8567974fb5bae73fe7563
Diana Zapalac <dlzapalac@gmail.com>
2021-03-21 20:50:15 +0000

    slowly adding new code

--- public/battleship.js
+++ public/battleship.js
@@ -1,8 +1,13 @@
-	var numWarnings = 0;
-        while ( numWarnings < 5) {
-	   alert("Still some warnings left")
-	   numWarnings++;
-	}
-	alert("All Your Warnings Have Been Used!")
-	console.log ("End Of battleship.js code")
+	var location1 = 4;
+	var location2 = 5;
+	var location3 = 6;
+
+	var guess;
+	var hits = 0;
+	var guesses = 0;
+
+        var isSunk = 0;
+
+	guess = prompt("Ready, aim, fire! (enter a number between 0-6):")
+	document.write ("You entered: " )
 
